2e97305553f05a4646776231bd32e809b6ed8af1
Peace Sandy <peacesandy04@gmail.com>
2024-04-30 11:55:10 +0100

    fixed copy link issue

--- shortner.js
+++ shortner.js
@@ -1,19 +1,25 @@
-$("#copy").click(function () {
-    $(".notify").toggleClass("active");
-    $("#notifyType").toggleClass("success");
-    
-    setTimeout(function(){
-      $(".notify").removeClass("active");
-      $("#notifyType").removeClass("success");
-    },2500);
-  });
+const inputBox = document.querySelector("#input");
+const submitButton = document.querySelector(".shorten");
+const outputButton = document.querySelector("#output");
+submitButton.addEventListener('click', submitUrl);
 
-  $(".shorten").click(function () {
-    $(".notify").toggleClass("active");
-    $("#notifyType").toggleClass("failed");
-    
-    setTimeout(function(){
-      $(".notify").removeClass("active");
-      $("#notifyType").removeClass("failed");
-    },1500);
-  });
+function submitUrl(e) {
+    let link = inputBox.value;
+    fetchData(link);
+}
+
+function fetchData(link) {
+    fetch('https://api-ssl.bitly.com/v4/shorten', {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Bearer e518510dfa4a9c16ebb1c164322e8183fa2f88fe',
+        'Content-Type': 'application/json'
+    },
+        body: JSON.stringify({ "long_url": link, "domain": "bit.ly" } )
+    }).then(res => res.json())
+    .then(result => setOutputValue(result.link))
+}
+
+function setOutputValue(value) {
+    outputButton.value = value;
+}
